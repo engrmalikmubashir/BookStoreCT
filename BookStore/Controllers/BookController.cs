41327e743128027b53da05a325eb28fdc8e70ae5
Muhammad Mubashir <57788343+engrmalikmubashir@users.noreply.github.com>
2020-04-09 19:05:14 +0500

    Updates

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+
+            return View(data);
+        }
+
+        public ViewResult GetBook(int id)
+        {
+            var data = _bookRepository.GetBookById(id);
+
+            return View(data);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
